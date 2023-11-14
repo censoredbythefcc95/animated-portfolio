6e5af962f2d629f9d8914f4452a53154a27470e3
Steven Young <sastudillo6@gmail.com>
2023-11-14 12:21:43 -0500

    Added framer-motion dependency, and animated name on navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from "framer-motion";
 import "./navbar.scss"
 
 const Navbar = () => {
@@ -6,7 +7,13 @@ const Navbar = () => {
         <div className="navbar">
             {/* Sidebar */}
             <div className="wrapper">
-                <span>Steven Young</span>
+                <motion.span
+                initial={{ opacity: 0, scale: 0.5}}
+                animate={{opacity: 1, scale: 1}}
+                transition={{ duration: 0.5}}
+                >
+                    Steven Young
+                </motion.span>
                 <div className="social">
                     <a href="#"><img src="/facebook.png" alt="" /></a>
                     <a href="#"><img src="/instagram.png" alt="" /></a>
