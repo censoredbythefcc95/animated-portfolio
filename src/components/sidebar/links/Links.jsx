52a9af606ba414f137cfdc44fea9249c78edf2a2
Steven Young <sastudillo6@gmail.com>
2023-11-14 18:04:08 -0500

    Added items array to Links component and mapped array to the sidebar.

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Links = () => {
+
+    const items = ["Homepage", "Services", "Portfolio", "Contact","About"]
+
     return (
         <div className="links">
-            Links
+            {items.map((item) =>(
+                <a href={`#${item}`} key={item}>
+                    {item}
+                </a>
+            ))}
         </div>
-    )
-}
+    );
+};
 
 export default Links; 
