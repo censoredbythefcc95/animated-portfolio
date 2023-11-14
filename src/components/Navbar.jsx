44a3bd1da136cc484078d6ae963132337abbbd7b
Steven Young <sastudillo6@gmail.com>
2023-11-14 13:16:43 -0500

    Div container social was losing flexbox property when wrapped in motion.span. Wrapped each image with motion instead. Resolved.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,16 +9,30 @@ const Navbar = () => {
             <div className="wrapper">
                 <motion.span
                 initial={{ opacity: 0, scale: 0.5}}
-                animate={{opacity: 1, scale: 1}}
-                transition={{ duration: 0.5}}
+                animate={{opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
                 >
                     Steven Young
                 </motion.span>
                 <div className="social">
-                    <a href="#"><img src="/facebook.png" alt="" /></a>
-                    <a href="#"><img src="/instagram.png" alt="" /></a>
-                    <a href="#"><img src="/youtube.png" alt="" /></a>
-                    <a href="#"><img src="/dribbble.png" alt="" /></a>
+                    <motion.a href="#"
+                    initial={{ opacity: 0, scale: 0.5}}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    ><img src="/facebook.png" alt="" /></motion.a>
+                    <motion.a href="#"
+                    initial={{ opacity: 0, scale: 0.5}}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    ><img src="/instagram.png" alt="" /></motion.a><motion.a href="#"
+                    initial={{ opacity: 0, scale: 0.5}}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    ><img src="/youtube.png" alt="" /></motion.a><motion.a href="#"
+                    initial={{ opacity: 0, scale: 0.5}}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    ><img src="/dribbble.png" alt="" /></motion.a>
                     </div>
                 </div>
             </div>
