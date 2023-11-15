ecaad7e12b5c518abb05a3f6b8a60bf61a10fc24
Steven Young <sastudillo6@gmail.com>
2023-11-14 21:00:43 -0500

    Animated front page.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,26 +1,42 @@
 import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        },
+    },
+};
 
 const Hero = () => {
     return (
-        <div className="hero">
-            <div className="wrapper">
-                <div className="textContainer">
-                    <h2>STEVEN ASTUDILLO</h2>
-                    <h1>Software developer and Technology Coordinator</h1>
-                    <div className="buttons">
-                        <button>See Latest Projects</button>
-                        <button>Let's chat</button>
-                    </div>
-                    <img src="/scroll.png" alt="" />
-                </div>
-            </div>
+        <motion.div className="hero">
+            <motion.div className="wrapper">
+                <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+                    <motion.h2 variants={textVariants}>STEVEN ASTUDILLO</motion.h2>
+                    <motion.h1 variants={textVariants}>Software developer and Technology Coordinator</motion.h1>
+                    <motion.div variants={textVariants} className="buttons">
+                        <motion.button variants={textVariants}>See Latest Projects</motion.button>
+                        <motion.button variants={textVariants}>Let's chat</motion.button>
+                    </motion.div>
+                    <motion.img variants={textVariants} src="/scroll.png" alt="" />
+                </motion.div>
+            </motion.div>
             <div className="slidingTextContainer">
                 Developer Operations Coordinator Writer
             </div>
             <div className="imageContainer">
                 <img src="/steven.png" alt="" />
             </div>
-        </div>
+        </motion.div>
     );
 };
 export default Hero;
