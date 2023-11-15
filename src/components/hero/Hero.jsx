9f52036a906e358c0eebca1fef18a26b064658b2
Steven Young <sastudillo6@gmail.com>
2023-11-14 20:32:53 -0500

    front page complete

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,20 +3,24 @@ import "./hero.scss";
 const Hero = () => {
     return (
         <div className="hero">
-            <div className="textContainer">
-                <h2>Steven Astudillo</h2>
-                <h1>Developer and Technology Coordinator</h1>
-                <div className="buttons">
-                    <button>See Latest Projects</button>
-                    <button>Let's talk</button>
+            <div className="wrapper">
+                <div className="textContainer">
+                    <h2>STEVEN ASTUDILLO</h2>
+                    <h1>Software developer and Technology Coordinator</h1>
+                    <div className="buttons">
+                        <button>See Latest Projects</button>
+                        <button>Let's chat</button>
+                    </div>
+                    <img src="/scroll.png" alt="" />
                 </div>
-                <img src="/scroll.png" alt="" />
+            </div>
+            <div className="slidingTextContainer">
+                Developer Operations Coordinator Writer
             </div>
             <div className="imageContainer">
-                <img src="steven.png" alt="" />
+                <img src="/steven.png" alt="" />
             </div>
         </div>
-    )
-}
-
+    );
+};
 export default Hero;
